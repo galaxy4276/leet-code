f7147397e32926d8ae4e1500842b3b589ef929cd
최은기 <galaxyhi4276@gmail.com>
2021-06-17 18:11:46 +0900

    refactor: jewelsAndStone refactor

--- difficulty/easy/jewelsAndStones.ts
+++ difficulty/easy/jewelsAndStones.ts
@@ -1,7 +1,5 @@
 /** @url https://leetcode.com/problems/jewels-and-stones/ */
 
-const numJewelsInStones = (jewels: string, stones: string) => {
-  return [...stones].filter((stone) => [...jewels].includes(stone)).length;
-};
+const numJewelsInStones = (jewels: string, stones: string) => [...stones].filter((stone) => [...jewels].includes(stone)).length;
 
 console.log(`numJewelsInStones Result: ${numJewelsInStones('aA', 'aAAbbbb')}`);
